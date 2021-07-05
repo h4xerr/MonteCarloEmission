e5854db85eb3a69f71ffde6fa0b9010d9e513ce7
H4xerr <h4xerr@gmail.com>
2021-07-05 21:42:41 +0300

    'test1'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+export default class App extends Component {
+  componentDidMount() {
+    console.log('I was triggered during componentDidMount')
+  }
 
+  render() {
+    console.log('I was triggered during render')
+    return (
+      <div> I am the App component </div>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
