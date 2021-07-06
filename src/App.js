8f1776fbf65e5b8f012b7b5b3c1a019664c61d69
H4xerr <h4xerr@gmail.com>
2021-07-06 10:22:48 +0300

    'test'

--- src/App.js
+++ src/App.js
@@ -5,21 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>MOMO E GLUP</h1>
+    <h1>TEST</h1>
     </div>
   );
-}
-
-export default class App extends Component {
-  componentDidMount() {
-    console.log('I was triggered during componentDidMount')
-  }
-
-  render() {
-    console.log('I was triggered during render')
-    return (
-      <div> I am the App component </div>
-    )
-  }
 }
 export default App;
